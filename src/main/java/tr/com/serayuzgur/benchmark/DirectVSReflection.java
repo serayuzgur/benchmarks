edcd913b433c9e75774c83a297fc982a2d13bca0
Seray Uzgur <seray.uzgur@mebitech.com>
2015-04-27 17:10:05 +0300

    new library added

--- src/main/java/tr/com/serayuzgur/benchmark/DirectVSReflection.java
+++ src/main/java/tr/com/serayuzgur/benchmark/DirectVSReflection.java
@@ -1,10 +1,11 @@
 package tr.com.serayuzgur.benchmark;
 
+import com.esotericsoftware.reflectasm.ConstructorAccess;
+import com.esotericsoftware.reflectasm.MethodAccess;
 import org.abstractmeta.reflectify.MethodInvoker;
 import org.abstractmeta.reflectify.Reflectify;
 import org.abstractmeta.reflectify.ReflectifyRegistry;
 import org.abstractmeta.reflectify.runtime.ReflectifyRuntimeRegistry;
-
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -28,25 +29,30 @@ public class DirectVSReflection {
         Reflectify.Provider provider = reflectify.getProvider();
 
 
-        Integer repeat = 10000;
+        Integer repeat = 100000;
 
+        //For reflectify
         MethodInvoker<Executable, Integer> invoker = reflectify.getMethodInvoker(Integer.class, methodname, Integer.class);
+
+        //For reflectasm
+        MethodAccess access = MethodAccess.get(Executable.class);
+
 
         //Warm up
         System.out.println("---Warming up---");
-        for (int i = 0; i < 3; i++) {
-            test(aClass, method, repeat, invoker,provider);
+        for (int i = 0; i < 11; i++) {
+            test(aClass, method, repeat, invoker,provider,access);
         }
 
         System.out.println("---Starting  up---");
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i < 21; i++) {
             System.out.println("----Pass" + i + "---");
-            test(aClass, method, repeat, invoker,provider);
+            test(aClass, method, repeat, invoker,provider,access);
         }
 
     }
 
-    private static void test(Class aClass, Method method, int repeat, MethodInvoker<Executable, Integer> invoker,Reflectify.Provider provider) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+    private static void test(Class aClass, Method method, int repeat, MethodInvoker<Executable, Integer> invoker,Reflectify.Provider provider,MethodAccess access) throws IllegalAccessException, InvocationTargetException, InstantiationException {
         long start = System.nanoTime();
         Integer result = 0;
         for (int i = 0; i < repeat; i++) {
@@ -66,6 +72,15 @@ public class DirectVSReflection {
             result  = (Integer) invoker.invoke((Executable) provider.get());
         }
         System.out.println(result + " Reflectify " + (System.nanoTime() - start) / 1000);
+
+
+        ConstructorAccess<Executable> clazzz = ConstructorAccess.get(Executable.class);
+        start = System.nanoTime();
+        for (int i = 0; i < repeat; i++) {
+            Executable someObject = clazzz.newInstance();
+            result = (Integer) access.invoke(someObject, "execute", repeat);
+        }
+        System.out.println(result + " Reflectasm " + (System.nanoTime() - start) / 1000);
 
     }
 
